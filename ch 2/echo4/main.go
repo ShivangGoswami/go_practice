f02de14f2aad809951e166d8b2dd416b2481d35d
ShivangGoswami <shivang.goswami@infosys.com>
2019-02-13 12:46:39 +0530

    popCount

--- ch 2/echo4/main.go
+++ ch 2/echo4/main.go
@@ -10,7 +10,7 @@ import (
 var n = flag.Bool("n", false, "omit trailing newline")
 var sep = flag.String("s", " ", "seperator")
 
-func main() {
+func main() {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
 	flag.Parse()
 	fmt.Print(strings.Join(flag.Args(), *sep))
 	if !*n {
