fc66c2080861e323641630e55b2503bfaf58f5dd
ShivangGoswami <shivang.goswami@infosys.com>
2019-02-14 16:40:16 +0530

    ch 3 almost

---
+++ ch3/basename1/main.go
@@ -0,0 +1,21 @@
+package basename
+
+//basename removes directory components and a .suffix
+//e.g. a+.a,a.go=>a,a/b/c.go=>c,a/b.c.go=>b.c
+func basename(s string) string {
+	//Discard last '/' and everything before.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+	//Preserve everything be4fore last '.'
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
