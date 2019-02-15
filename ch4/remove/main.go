ccd7c26f35db54312618fa3f80b97ec335e08c08
ShivangGoswami <shivang.goswami@infosys.com>
2019-02-15 18:32:46 +0530

    pg 9

---
+++ ch4/remove/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func remove(slice []int, i int) []int {
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1]
+}
+
+func remove1(slice []int, i int) []int {
+	slice[i] = slice[len(slice)-1]
+	return slice[:len(slice)-1]
+}
+func main() {
+	s := []int{5, 6, 7, 8, 9}
+	fmt.Println(remove1(s, 2))
+}
