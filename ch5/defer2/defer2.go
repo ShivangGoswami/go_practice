06ed538f6c1a98308708ee93ecffa2c0bface25a
Prachi Shekhar -X (pshekhar - INFOSYS LIMITED at Cisco) <pshekhar@partnet.cisco.com>
2019-03-14 15:06:47 +0530

    chapter 5 done

--- ch5/defer2/defer2.go
+++ ch5/defer2/defer2.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"runtime"
 )
@@ -13,5 +14,11 @@ func main() {
 func printStack() {
 	var buf [4096]byte
 	n := runtime.Stack(buf[:], false)
-	os.Stdout.Write(buf[:])
+	os.Stdout.Write(buf[:n])
+}
+
+func f(x int) {
+	fmt.Printf("f(%d)\n", x+0/x)
+	defer fmt.Printf("defer %d\n", x)
+	f(x - 1)
 }
