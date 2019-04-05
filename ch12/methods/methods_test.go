42522b7b930b4410cf9ef0d7c3aa9f20f2cb00bc
ShivangGoswami <shivang.goswami@infosys.com>
2019-04-05 15:34:47 +0530

    359

--- ch12/methods/methods_test.go
+++ ch12/methods/methods_test.go
@@ -2,11 +2,16 @@ package methods_test
 
 import (
 	"strings"
+	"testing"
 	"time"
 
 	"../methods"
 )
 
+func Test(t *testing.T) {
+	methods.Print(time.Hour)
+	methods.Print(new(strings.Replacer))
+}
 func ExamplePrintDuration() {
 	methods.Print(time.Hour)
 }
