a96431ae43eadc4f3ad23996ddfd5f080ebbdf1f
ShivangGoswami <shivang.goswami@infosys.com>
2019-04-05 19:15:37 +0530

    lst pg

--- ch13/bzip/bzip2.c
+++ ch13/bzip/bzip2.c
@@ -5,9 +5,9 @@ int bx2compress(bz_stream *s,int action,char *in,unsigned *inlen,char *out,unsig
     s->avail_in=*inlen;
     s->next_out=out;
     s->avail_out=*outlen;
-    int r=BZ@_bzCompress(s,action);
+    int r=BZ2_bzCompress(s,action);
     *inlen-=s->avail_in;
     *outlen-=s->avail_out;
-    s->next_in=s->next_out=NULL:
-    return r
+    s->next_in=s->next_out=NULL;
+    return r;
 }
--- ch13/bzip/bzip2.go
+++ ch13/bzip/bzip2.go
@@ -1,13 +1,16 @@
 package bzip
 
 /*
-#cgo CFLAGS: -I/usr/include
-#cgo LDFLAGS: -L/usr/lib -lbz2
+//#cgo CFLAGS: -I/usr/include
+//#cgo LDFLAGS: -L/usr/lib -lbz2
 #include<bzlib.h>
 int bz2compress(bz_stream *s,int action,char *in,unsigned *intlen,char *out,unsigned *outlen);
 */
-import "C"
-import "io"
+import (
+	"unsafe"
+	"C"
+	"io"
+)
 
 type Writer struct {
 	w      io.Writer
@@ -22,6 +25,45 @@ func NewWriter(out io.Writer) io.WriteCloser{
 		workFactor=30
 	)
 	w:=&Writer{w:out,stream:new(c.bz_stream)}
-	c.Bz2_bzCompressInit(w.stream,blockSize,verbosity,workFactor)
+	C.Bz2_bzCompressInit(w.stream,blockSize,verbosity,workFactor)
 	return w
+}
+
+func (w *writer) Write(data []byte) (int,error) {
+	if w.stream == nil{
+		panic("closed")
+	}
+	var total int
+	for len(data)>0 {
+		inlen,outlen:=C.uint(len(data),C.uint(cap(w.outbuf)))
+		c.bz2compress(w.stream,c.BZ_RUN,
+			(*C.char)(unsafe.Pointer(&data[0])),&inlen,
+			(*C.char)(unsafe.Pointer(&w.outbuf)),&outlen)
+		total+=int(inlen)
+		data=data[inlen:]
+		if _,err:=w.w.Write(w.outbuf[:outlen]);err!=nil{
+			return total,err
+		}
+	}
+	return total,nil
+}
+
+func (w *writer) Close() error{
+	if w.stream==nil{
+		panic("closed")
+	}
+	defer func(){
+		C.Bz2_bzCompressInit(w.stream)
+		w.stream=nil
+	}()
+	for {
+		inlen,outlen:=C.uint(0),C.uint(cap(w.outbuf))
+		r:=C.bz2compress(w.stream,C.BZ_FINISH,nil,&inlen,(*C.char)(unsafe.Pointer(&w.outbuf)),&outlen)
+		if _,err:=w.w.write(w.outbuf[:outlen]);err!=nil{
+			return err
+		}
+		if r==C.BZ_STREAM_END{
+			return nil
+		}
+	}
 }
